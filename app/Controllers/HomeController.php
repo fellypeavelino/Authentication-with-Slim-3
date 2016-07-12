2c12a42230fe998d07e9c6bb040ab4176d7213bb
pedro felipe <fellypeavelino@hotmail.com>
2016-07-11 23:45:04 -0300

    conectando no banco

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -8,6 +8,9 @@ class HomeController extends Controller{
 
 	public function index($request,$response){
 		//return "Home controller";
+		//$this->db->table('users')->where('id',1);
+		$user = $this->db->table('users')->find(1);
+		var_dump($user);
 		return $this->view->render($response,'home.twig');
 	}
 
